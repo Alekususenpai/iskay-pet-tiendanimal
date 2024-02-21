5ee68702beb4fe8007cc86a9e8226ce7d5e245d9
Alekususenpai <aa_lara@live.com>
2024-02-20 22:10:24 -0500

    Project basic setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Header from './components/layout/Header'
+import Navigation from './pages/Navigation'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Navigation />
+    </>
+  )
+}
+
+export default App

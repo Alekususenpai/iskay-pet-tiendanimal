a87dac34dcd87f842fe3566447ec8422e68fb657
Alekususenpai <aa_lara@live.com>
2024-02-22 09:54:39 -0500

    Todo - rest api fetching todos / State management with Redux Toolkit

--- src/components/tasks/TaskItem.tsx
+++ src/components/tasks/TaskItem.tsx
@@ -1,14 +1,23 @@
 import React from 'react'
+import { useDispatch } from 'react-redux';
+import { deleteTaskAsync } from '../../store/taskSlice';
 import trashIcon from '../../assets/Trash.svg'
 
-function TaskItem() {
+function TaskItem({ task }) {
+
+          const dispatch = useDispatch();
+
+          const handleDelete = () => {
+                    dispatch(deleteTaskAsync(task.id));
+          };
+
           return (
-                    <div className='bg-white px-4 py-5 shadow-md rounded-md flex justify-between'>
+                    <div className='bg-white px-4 py-5 my-4 shadow-md rounded-md flex justify-between'>
                               <div>
-                                        <h3 className='text-lg font-base'>Titulo Area</h3>
-                                        <p className='font-light text-gray-400 text-sm'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris, ac elementum ultrices mauris. Cursus urna</p>
+                                        <h3 className='text-lg font-base'>{task.userId || 'Titulo Area'}</h3>
+                                        <p className='font-light text-gray-400 text-sm'>{task.title || 'Descripcion de la tarea'}</p>
                               </div>
-                              <img src={trashIcon} alt="Delete" className="w-5 ml-2" />
+                              <img src={trashIcon} alt="Delete" className="w-5 ml-2" onClick={handleDelete} />
                     </div>
           )
 }
